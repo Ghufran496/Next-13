5a595083dbda419fd38cd620ca33926696704c8f
Ghufran Ullah <92268694+Ghufran496@users.noreply.github.com>
2024-03-20 12:45:15 +0500

    course done

--- app/conference/speakers/page.js
+++ app/conference/speakers/page.js
@@ -1,12 +1,87 @@
+import styles from "../conference.module.css";
 import Link from "next/link";
-export default function Page() {
-    return (
-      <>
-        <h1>Welcome to Speakers Page</h1>
-        <h2>
-          <Link href="/conference">Back to conference</Link>
-        </h2>
-      </>
-    );
-  }
-  
+
+export let speakerJson = {};
+
+// Static data fetching
+async function fetchSpeakers() {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json"
+  );
+
+  const data = await response.json();
+  speakerJson = data;
+  return data;
+}
+
+export default async function Page() {
+  const data = await fetchSpeakers();
+
+  return (
+    <div className={styles.parentContainer}>
+      <div className="self-start whitespace-nowrap rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
+        Last Rendered: {new Date().toLocaleTimeString()}
+      </div>
+      <h1>Welcome to Globomantics Speakers</h1>
+      {data.speakers.map(({ id, name, bio }) => (
+        <div key={id} className={styles.infoContainer}>
+          <Link
+            className={styles.bgLinks}
+            href={`/conference/speakers/${name}`}
+          >
+            <h3 className={styles.titleText}>{name}</h3>
+          </Link>
+          <h5 className={styles.descText}>{bio}</h5>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+
+// import Link from "next/link";
+// import styles from "../conference.module.css";
+
+// //static datafetch no fetched from cache not updated again n again
+// // async function fetchSpeakers() {
+// //   const response = await fetch(
+// //     "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json"
+// //   );
+
+// //   const data = await response.json();
+// //   return data;
+// // }
+
+// //static datafetching with revalidation
+
+// async function fetchSpeakers() {
+//   const response = await fetch(
+//     "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json",
+//     { next: { revalidate: 20 } }
+//   );
+
+//   const data = await response.json();
+//   return data;
+// }
+
+// export default async function Page() {
+//   const data = await fetchSpeakers();
+
+//   return (
+//     <div className={styles.parentContainer}>
+//       <div className="self-start whitespace-nowrap rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
+//         Last Rendered: {new Date().toLocaleTimeString()}
+//       </div>
+//       <h1>Welcome to Globomantics Speakers</h1>
+//       {data.speakers.map(({ id, name, bio }) => (
+//         <div key={id} className={styles.infoContainer}>
+//           <Link href={`/conference/speakers/${name}`}>
+//             <h3 className={styles.titleText}>{name}</h3>
+//           </Link>
+
+//           <h5 className={styles.descText}>{bio}</h5>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
